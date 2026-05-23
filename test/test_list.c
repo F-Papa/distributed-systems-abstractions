b94d7d4e18607f28071e524b5cbaf21ead92b8ad
F-Papa <francopapa21@gmail.com>
2026-05-23 14:19:11 +0000

    chore: organize project in directories

---
+++ test/test_list.c
@@ -0,0 +1,14 @@
+#include "list.h"
+
+int main() {
+  list_t *l = list_init();
+  int uno = 1, dos = 2, tres = 3;
+  list_add(l, &uno);
+  list_add(l, &dos);
+  list_add(l, &tres);
+
+  printf("%d\n", list_index(l, &uno));
+  printf("%d\n", list_index(l, &dos));
+  printf("%d\n", list_index(l, &tres));
+  printf("%d\n", list_index(l, NULL));
+}
