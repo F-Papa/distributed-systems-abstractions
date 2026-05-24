e93d5391ed51968a814475771c781eb2f8f1fe0d
F-Papa <francopapa21@gmail.com>
2026-05-24 01:17:43 +0000

    add typedef for struct AuthPerfectLink

--- include/link/auth_perfect_link.h
+++ include/link/auth_perfect_link.h
@@ -11,12 +11,13 @@ typedef struct {
   PlDeliver base;
 } AuthPlDeliver;
 
-struct AuthPerfectLink;
+typedef struct AuthPerfectLink Apl;
 
-struct AuthPerfectLink *apl_init(int id, int retransmission_period,
-                                 const unsigned char private_key[crypto_sign_SECRETKEYBYTES],
-                                 int max_rank,
-                                 const unsigned char public_keys[][crypto_sign_PUBLICKEYBYTES]);
+struct AuthPerfectLink *
+apl_init(int id, int retransmission_period,
+         const unsigned char private_key[crypto_sign_SECRETKEYBYTES],
+         int max_rank,
+         const unsigned char public_keys[][crypto_sign_PUBLICKEYBYTES]);
 
 int apl_send(struct AuthPerfectLink *apl, AuthPlSend *e);
 
